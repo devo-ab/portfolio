c39d7689bb58d756e27f1484478d5e03b82d1599
designer-ab <per.useravi0@gmail.com>
2024-06-24 20:28:54 +0600

    complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Shared/Navbar/Navbar";
+import Footer from "@/components/Shared/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="portfolioThemes">
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Portfolio
-      <button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
+    <main>
+      
     </main>
   );
 }
