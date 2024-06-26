bc0c829f875ab3fb7354bcf9a25c37023a206163
designer-ab <per.useravi0@gmail.com>
2024-06-26 11:55:09 +0600

    added navbar with responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import PortHome from "@/components/PortHome/PortHome";
+
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
     <main>
-      
+      <PortHome></PortHome>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,19 +17,4 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
-  daisyui: {
-    themes: [
-      {
-        portfolioThemes: {
-          "primary": "#a991f7",
-          "secondary": "#f6d860",
-          "accent": "#37cdbe",
-          "neutral": "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-      "dark",
-      "cupcake",
-    ],
-  },
 };
