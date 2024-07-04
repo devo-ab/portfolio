4862c889920eea63ac413d977426dff6c8e4d4bd
designer-ab <per.useravi0@gmail.com>
2024-07-04 13:04:15 +0600

    added contact with responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,115 @@
-"use client"
+"use client";
 import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const message = e.target.message.value;
+    console.log(name, email, message)
+
+  };
+
   return (
     <div id="contact">
       <div className="mt-10">
         <p className="text-center text-lg">Get In Touch</p>
         <h1 className="text-4xl font-bold text-center">Contact Me</h1>
+
+        <div className="flex flex-col md:flex-row items-center gap-10 mt-8">
+          <div className="flex-1">
+            <div>
+              <h1 className="text-4xl font-bold">Get in touch</h1>
+              <p className="mt-2">
+                Let's collaborate on something great! Whether you're looking to start a new project
+                or need assistance with an existing one, I'm eager to hear from you. Reach out
+                today, and let's make something awesome together!
+              </p>
+              <div className="mt-2">
+                <p className="flex gap-2 items-center">
+                  <FaPhoneAlt />
+                  +8801312291092
+                </p>
+                <p className="flex gap-2 items-center">
+                  {" "}
+                  <MdEmail />
+                  avijit0x@gmail.com
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <a href="https://github.com/devo-ab" target="_blank">
+                  <p className="text-2xl">
+                    <FaGithub />
+                  </p>
+                </a>
+                <a href="www.linkedin.com/in/imavijit" target="_blank">
+                  <p className="text-2xl">
+                    <FaLinkedin />
+                  </p>
+                </a>
+                <a href="https://wa.link/gzpn6b" target="_blank">
+                  <p className="text-2xl">
+                    <FaWhatsapp />
+                  </p>
+                </a>
+                <a href="https://www.facebook.com/imavijit0" target="_blank">
+                  <p className="text-2xl">
+                    <FaFacebook />
+                  </p>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-1">
+            <form onSubmit={handleSubmit} noValidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
+              <label className="block">
+                <span className="mb-1">Full name</span>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  required
+                  placeholder="Write your name"
+                  className="block w-full rounded-md shadow-sm px-4 py-2 border border-gray-500"
+                />
+              </label>
+              <label className="block">
+                <span className="mb-1">Email address</span>
+                <input
+                name="email"
+                  type="email"
+                  id="email"
+                  required
+                  placeholder="Write your email"
+                  className="block w-full rounded-md shadow-sm px-4 py-2 border border-gray-500"
+                />
+              </label>
+              <label className="block">
+                <span className="mb-1">Message</span>
+                <textarea
+                name="message"
+                id="message"
+                placeholder="Write your message"
+                  rows="3"
+                  className="block w-full rounded-md px-4 py-2 border border-gray-500"
+                ></textarea>
+              </label>
+              <button
+                type="submit"
+                className="px-8 py-3 text-lg rounded bg-[#219C90] btn btn-ghost text-white"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
